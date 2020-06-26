fa4d056e0f5414e9bda1005fc2701fd7e4cb2f89
Armand Wayoff <46006060+armandwayoff@users.noreply.github.com>
2020-06-26 13:20:28 +0200

    Create solution.py

---
+++ HackerRank/Bigger is Greater (Medium)/solution.py
@@ -0,0 +1,22 @@
+# The 'next_permutation' function comes from https://www.nayuki.io/page/next-lexicographical-permutation-algorithm
+
+def biggerIsGreater(w):
+    return next_permutation(list(w))
+
+def next_permutation(arr):
+    # Find non-increasing suffix
+    i = len(arr) - 1
+    while i > 0 and arr[i - 1] >= arr[i]:
+        i -= 1
+    if i <= 0:
+        return 'no answer'
+    
+    # Find successor to pivot
+    j = len(arr) - 1
+    while arr[j] <= arr[i - 1]:
+        j -= 1
+    arr[i - 1], arr[j] = arr[j], arr[i - 1]
+    
+    # Reverse suffix
+    arr[i : ] = arr[len(arr) - 1 : i - 1 : -1]
+    return ''.join(arr)
