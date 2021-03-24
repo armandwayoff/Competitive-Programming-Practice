b8bf1d9837c0d613a1a0aaea0a930bd2d12cfcab
Armand Wayoff <armandwayoff@lavabit.com>
2021-03-24 21:20:18 +0100

    reformat

--- HackerRank/The Power Sum (Medium)/solution.py
+++ HackerRank/The Power Sum (Medium)/solution.py
@@ -1,11 +1,10 @@
 # Solution from https://nerdprogrammer.com/hackerrank-the-power-sum-solution/
 
-def powerSum(X, N, current = 1):
+def powerSum(X, N, current=1):
     pw = pow(current, N)
     if pw > X:
         return 0
     elif pw == X:
         return 1
     else:
-        return powerSum(X, N, current+1) + powerSum(X-pw, N, current+1)
-      
+        return powerSum(X, N, current + 1) + powerSum(X - pw, N, current + 1)
